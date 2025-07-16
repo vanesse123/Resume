fb7d1fcb8f7d5cec489d3a3156947b39f2f42418
vanesse123 <vanesse13579@gmail.com>
2025-07-16 21:27:51 +0800

    專案頁面css完成 + js切換頁面淡出淡入

--- javascript/index.js
+++ javascript/index.js
@@ -1,18 +1,29 @@
 
 function loadPage(page) {
-    fetch(`temphtml/${page}.html`)
-        .then(response => {
-        if(!response.ok) {
-            throw new Error(`載入失敗:${response.status}`);
-        }
-        return response.text();
-        })
-        .then(html => {
-            document.getElementById('main-content').innerHTML = html;
-        })
-        .catch(error =>{
-            document.getElementById('main-content').innerHTML = `<p style="color:red;">`
-        });
+    const content = document.getElementById('main-content');
+    content.classList.add('hidden');
+    
+    setTimeout(() => {
+        fetch(`temphtml/${page}.html`)
+            .then(response => {
+            if(!response.ok) {
+                throw new Error(`載入失敗:${response.status}`);
+            }
+            return response.text();
+            })
+            .then(html => {
+                content.innerHTML = html;
+                requestAnimationFrame(() => {
+                    requestAnimationFrame(() => {
+                        content.classList.remove('hidden'); // 再淡入
+                    });
+                });
+            })
+            .catch(error =>{
+                document.getElementById('main-content').innerHTML = `<p style="color:red;"></p>`
+                content.classList.remove('hidden');
+            });
+    },600);
 }
 
-window.onload = () => loadPage('resume');
+window.onload = () => loadPage('projects');
